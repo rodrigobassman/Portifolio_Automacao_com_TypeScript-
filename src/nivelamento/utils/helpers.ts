ee9400aba5c4560f101d7955973494ea7b5488b2
rodrigobassman <autor.rodrigo.araujo@gmail.com>
2026-09-10 14:12:56 +0000

    executando teste

--- src/nivelamento/utils/helpers.ts
+++ src/nivelamento/utils/helpers.ts
@@ -2,32 +2,36 @@
 function aguardar(ms: number): Promise<void> {
     return new Promise((resolve) => setTimeout(resolve, ms));
 }
-//SIMULANDO UMA API LOGIN
-function simularlogin(usuario:string, senha:string): Promise<string>{
+
+// SIMULANDO UMA API LOGIN
+function simularlogin(usuario: string, senha: string): Promise<string> {
     return new Promise((resolve, reject) => {
-        if(usuario === "admin" && senha === "123456"){
+        if (usuario === "admin" && senha === "123456") {
             resolve("Login bem-sucedido!");
         } else {
             reject("Usuário ou senha inválidos.");
         }
-
     });
 }
-//FUNÇÃO PRNCIPAL TESTANDO COM ASYNC/AWAIT
+
+// FUNÇÃO PRINCIPAL TESTANDO COM ASYNC/AWAIT
 async function executarCT() {
     console.log("Iniciando cenário de teste...");
-    try{
+    try {
         console.log("passo 1: abrindo tela de login");
         await aguardar(2000);
+        
         console.log("passo 2: inserindo credenciais");
         await aguardar(3000);
-        const token =await simularlogin("admin", "123456");
-        console.log('SUCESSO! USUARIO LOGADO, TOKEN RECEBIDO: ${token}\n');
+        
+        const token = await simularlogin("admin", "123456");
+        console.log(`SUCESSO! USUARIO LOGADO, TOKEN RECEBIDO: ${token}\n`);
     } catch (erro) {
-        console.error("FALHA NO TESTE ${erro}\n");
+        console.error(`FALHA NO TESTE: ${erro}\n`);
     } finally {
-        console.log('Passo final: fechando navegador e ligando dados.');
-
-       
+        console.log("Passo final: fechando navegador e limpando dados.");
     }
 }
+
+// Chamando a função para executar
+executarCT();
