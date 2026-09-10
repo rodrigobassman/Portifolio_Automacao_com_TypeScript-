32d5b781f29ec29a27c5bd7213bb09fd318122e9
rodrigobassman <autor.rodrigo.araujo@gmail.com>
2026-09-10 13:33:50 +0000

    Sua mensagem de commit

---
+++ src/nivelamento/pratica-assync.ts
@@ -0,0 +1,2 @@
+//importando a função utilitária de aguardar tempo(delay)
+import { aguardar } from "./utils/helpers";
--- src/nivelamento/utils/helpers.ts
+++ src/nivelamento/utils/helpers.ts
@@ -1,2 +1,29 @@
-//fução delay para retornar promises
-function
+// Função auxiliar para aguardar
+function aguardar(ms: number): Promise<void> {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+//SIMULANDO UMA API LOGIN
+function simularlogin(usuario:string, senha:string): Promise<string>{
+    return new Promise((resolve, reject) => {
+        if(usuario === "admin" && senha === "1234"){
+            resolve("Login bem-sucedido!");
+        } else {
+            reject("Usuário ou senha inválidos.");
+        }
+
+    });
+}
+//FUNÇÃO PRNCIPAL TESTANDO COM ASYNC/AWAIT
+async function executarCT() {
+    console.log("Iniciando cenário de teste...");
+    try{
+        console.log("passo 1: abrindo tela de login");
+        await aguardar(2000);
+        console.log("passo 2: inserindo credenciais");
+        await aguardar(2000);
+        console.log("passo 3: clicando no botão de login");
+        await aguardar(2000);
+        const resultado = await simularlogin("admin", "1234");
+        console.log(resultado);
+    }
+}
