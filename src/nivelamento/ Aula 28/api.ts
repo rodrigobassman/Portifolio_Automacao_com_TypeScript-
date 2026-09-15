a7e6bff28fa716aed28e6a4f0371bd0879628afa
rodrigobassman <autor.rodrigo.araujo@gmail.com>
2026-09-15 13:20:35 +0000

    Aula 28.1

---
+++ src/nivelamento/ Aula 28/api.ts
@@ -0,0 +1,16 @@
+const BASE_URL = 'https://jsonplaceholder.typicode.com/';
+// Definir contratos de tipo
+type Post = {
+ userId:number;
+ id?: number; // Campo opcional
+ tittle: string;
+ body: string;
+};
+type comment ={
+postId: number;
+id: number;
+name: string;
+email: string;
+body: string;
+
+}
