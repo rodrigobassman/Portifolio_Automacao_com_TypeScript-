6d9deea2ff4e6f6a24caf67241d76bdc8613e0d0
rodrigobassman <autor.rodrigo.araujo@gmail.com>
2026-09-15 13:50:46 +0000

    aula28.3

--- src/nivelamento/ Aula 28/api.ts
+++ src/nivelamento/ Aula 28/api.ts
@@ -1,21 +1,36 @@
-const BASE_URL = 'https://jsonplaceholder.typicode.com/';
+const BASE_URL = 'https://jsonplaceholder.typicode.com';
+
 // Definir contratos de tipo
 type Post = {
- userId:number;
- id?: number; // Campo opcional
- tittle: string;
- body: string;
-};
-type comment ={
-postId: number;
-id: number;
-name: string;
-email: string;
-body: string;
+  userId: number;
+  id?: number; // Campo opcional
+  title: string; // Corrigido de 'tittle' para 'title'
+  body: string;
 };
 
-//GET /posts
-async function listarPosts(){
-    
-    
+type Comment = { // Recomendado usar PascalCase, mas 'comment' é aceito
+  postId: number;
+  id: number;
+  name: string;
+  email: string;
+  body: string;
+};
+
+// GET /posts
+async function listarPosts() {
+  console.log('--- 1. GET /posts ---');
+  
+  // Corrigido para usar crases (``)
+  const res = await fetch(`${BASE_URL}/posts`);
+  const dados: Post[] = await res.json();
+  
+  // Corrigido para usar crases e propriedades corretas
+  console.log(`status: ${res.status}`);
+  console.log(`Lidos ${dados.length} posts. EX: do primeiro`, dados[0]?.title);
 }
+
+async function chamarReqs() {
+  await listarPosts();
+}
+
+chamarReqs();
