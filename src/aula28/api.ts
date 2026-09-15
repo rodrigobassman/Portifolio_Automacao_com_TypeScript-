d6897c9c1b621d4ba42797493029f8fcf0d64f3e
rodrigobassman <autor.rodrigo.araujo@gmail.com>
2026-09-15 15:00:32 +0000

    aula28.5

--- src/aula28/api.ts
+++ src/aula28/api.ts
@@ -34,3 +34,72 @@ async function chamarReqs() {
 }
 
 chamarReqs();
+
+//GET /posts/1
+async function buscarPorId(id:number) {
+    // GET /posts
+async function listarPosts() {
+  console.log('--- 1. GET /posts ---');
+  
+  // Corrigido para usar crases (``)
+  const res = await fetch(`${BASE_URL}/posts/${id}`);
+  const dados: Post[] = await res.json();
+  
+  // Corrigido para usar crases e propriedades corretas
+  console.log(`status: ${res.status}`);
+  console.log(`titulo do post: ${id}:', dados.title);
+
+async function chamarReqs() {
+  listarPosts();
+  buscarPorId(3);
+}
+
+chamarReqs();
+  }
+
+const BASE_URL = 'https://jsonplaceholder.typicode.com';
+
+// Definir contratos de tipo
+type Post = {
+  userId: number;
+  id?: number;
+  title: string;
+  body: string;
+};
+
+type Coment = {
+  postId: number;
+  id: number;
+  name: string;
+  email: string;
+  body: string;
+};
+
+// GET /posts
+async function listarPosts() {
+  console.log('--- 1. GET /posts ---');
+  const res = await fetch(`${BASE_URL}/posts`);
+  const dados: Post[] = await res.json();
+  console.log(`status: ${res.status}`);
+  console.log(`Lidos ${dados.length} posts. EX: do primeiro`, dados[0]?.title);
+}
+
+// GET /posts
+async function listarComent(postID:number) {
+  console.log('--- 3. GET /posts ---');
+  const res = await fetch(`${BASE_URL}/posts`);
+  const dados: Post[] = await res.json();
+  console.log(`status: ${res.status}`);
+  console.log(`O post ${postId} tem ${dados.length} comentários. EX: Email do primeiro comentário:`, dados[0]?.email);
+}
+
+// Função principal de execução
+async function chamarReqs() {
+  await listarPosts();
+  await listarComent(1);
+}
+
+chamarReqs();
+
+
+
